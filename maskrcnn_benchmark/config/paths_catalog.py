bb386e4ac5512e1e755335861775812ad25c23d6
zouzhaofan <zouzhaofan@outlook.com>
2019-07-17 16:29:13 +0800

    add coco2017 datasets

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -52,12 +52,12 @@ class DatasetCatalog(object):
             "ann_file":"Sensing/Sensing_coperson_test/labels"
         },
         "coco_2017_train": {
-            "img_dir": "coco/train2017",
-            "ann_file": "coco/annotations/instances_train2017.json"
+            "img_dir": "coco2017/train2017",
+            "ann_file": "coco2017/annotations/instances_train2017.json"
         },
         "coco_2017_val": {
-            "img_dir": "coco/val2017",
-            "ann_file": "coco/annotations/instances_val2017.json"
+            "img_dir": "coco2017/val2017",
+            "ann_file": "coco2017/annotations/instances_val2017.json"
         },
         "coco_2014_train": {
             "img_dir": "coco/train2014",
