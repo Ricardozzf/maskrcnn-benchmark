6baedcc09f9fd1eec27e0fda3d2bb989a3ef94c9
zouzhaofan <zouzhaofan@outlook.com>
2020-04-14 20:33:56 +0800

    change SGD to Adam

--- maskrcnn_benchmark/solver/build.py
+++ maskrcnn_benchmark/solver/build.py
@@ -16,7 +16,8 @@ def make_optimizer(cfg, model):
             weight_decay = cfg.SOLVER.WEIGHT_DECAY_BIAS
         params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
 
-    optimizer = torch.optim.SGD(params, lr, momentum=cfg.SOLVER.MOMENTUM)
+    #optimizer = torch.optim.SGD(params, lr, momentum=cfg.SOLVER.MOMENTUM)
+    optimizer = torch.optim.Adam(params, lr)
     return optimizer
 
 
